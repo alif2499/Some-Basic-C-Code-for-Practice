a357e0abcdb08b3dded080fb2218285d97c4aa0d
Alif_Rahman <alifjcc99@gmail.com>
2021-04-13 17:39:18 +0600

    Add files via upload

---
+++ Head Tail Code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main (void)
+{
+    int j,i,ct[60];
+    srand(time(NULL));
+    i=rand();
+    j=i%2;
+    if (j==0)
+    printf("Head");
+
+    else
+        printf("Tail");
+
+}
+
