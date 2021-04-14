b5b38759ca6f806f8c1705512683388a0faa0c13
Alif_Rahman <alifjcc99@gmail.com>
2021-04-14 13:19:05 +0600

    Converts into taka from dollar and rupee

---
+++ Currency Conversion 2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+ main ()
+{
+    int dollar,taka,rupee,num;
+    char (1),(2);
+
+    printf("Enter amount in taka: \n");
+    scanf("%d",&taka);
+
+    printf("Enter num: \n");
+    scanf("%c",&num;
+
+    if(num='1'){
+        printf("your amount in Dollar is:%d",taka/80);
+    }
+    else if(num='2'){
+        printf("your amount in rupee is:%d",taka/2);
+    }
+}
