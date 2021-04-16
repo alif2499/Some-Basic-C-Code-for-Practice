8edb78e17a39727111132b44d24c1560d238f872
Alif_Rahman <alifjcc99@gmail.com>
2021-04-16 19:50:24 +0600

    Add files via upload

---
+++ summation.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y,z;
+    printf("Enter vlue of x\n");
+    scanf("%d",&x);
+    printf("Enter value of y\n");
+    scanf("%d",&y);
+    z=x+y;
+    printf("The value of %d+%dequals%d\n",x,y,z);
+
+    return 0;
+}
