81a4ba4a29f375dbd43d95f1d142b4888da425c0
Alif_Rahman <alifjcc99@gmail.com>
2021-05-25 11:15:13 +0600

    Add files via upload

---
+++ celcius.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<float.h>
+
+void main()
+
+{
+    float c;
+    int f;
+    printf("enter tempeture in f ");
+
+    scanf("%d",&f);
+    c=5*(f-32)/9;
+
+    printf("temperature in c=%f",c);
+
+
+
+}
