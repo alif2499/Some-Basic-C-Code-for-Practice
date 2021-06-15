f547aa03f1752289f7da1e8ffdec58ad8b00c083
Alif_Rahman <alifjcc99@gmail.com>
2021-06-16 01:47:25 +0600

    Add files via upload

---
+++ flexiload 2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void main (void)
+{
+    int i,j,r,m,roll,ct[60];
+    srand(time(NULL));
+
+    for(m=0;m<100;m++){
+/////////////////////////////
+    for(i=0;i<16;i++){
+            j=rand()%10;
+        printf("%c",j+48);
+
+
+}
+////////////////////////
+
+printf("\n");
+}
+}
+
+
+
