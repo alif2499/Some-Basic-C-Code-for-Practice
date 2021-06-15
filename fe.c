cea23e2677d9e420d109551c92deb4cd39267a6f
Alif_Rahman <alifjcc99@gmail.com>
2021-06-16 01:46:15 +0600

    Add files via upload

---
+++ fe.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+char x;
+printf("Enter your name\n");
+scanf("%c", &x);
+if (x=='alif') {
+printf("tui akta DHON");
+}
+else {
+printf("Valo sele");
+}
+return 0;
+}
