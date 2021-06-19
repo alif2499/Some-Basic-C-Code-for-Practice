d7996ae9786c01957476e8e85237274456c8e603
Alif_Rahman <alifjcc99@gmail.com>
2021-06-19 11:07:00 +0600

    Add files via upload

---
+++ function.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int factorial(int n);
+int main ()
+{
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    printf("factorial of %d=%d",n,factorial(n));
+    return 0;
+}
+int factorial(int n)
+{
+     if(n!=1)
+
+        return (n*factorial(n-1));
+}
